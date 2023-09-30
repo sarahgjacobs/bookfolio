45df0029dce5d0183a031c5a0b9f257c969dd045
Sarah Jacobs <sgjacobs14@gmail.com>
2023-09-30 13:02:35 -0400

    added navbar

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import { Carousel } from "@material-tailwind/react";
+import thecircuit from '../images/thecircuit.jpeg';
+import inheritance from '../images/inheritance.jpeg';
+import levelup from '../images/levelup.jpg';
+ 
+export default function Home() {
+  return (
+    <div>
+    
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+
+    return (
+        <div className='text-black flex justify-between items-center h-24 max-w-[1240px] px-4'>
+            <h1 className='w-full text-3xl font-bold text-[#48d3e8]'>Bookfolio.</h1>
+            <ul className='flex pt-6 uppercase'>
+                <li className='p-4 '>Home</li>
+                <li className='p-4'>About Sarah</li>
+                <li className='p-4'>The Circuit</li>
+                <li className='p-4'>Inheritance</li>
+                <li className='p-4'>Advice for Killers</li>
+                <li className='p-4'>Commendation for Reforia</li>
+                <li className='p-4'>Level Up</li>
+            </ul>
+            
+            
+            </div>
+        
+    )
+}
+
+
+export default Navbar
